2eec5735eceb28e27e28cc41b2395b40d344d152
Paul Breakthrough <paulbreakthru@outlook.com>
2020-01-15 13:02:33 +0100

    Added Person Object

--- BankApp/src/bankapp/BankApp.java
+++ BankApp/src/bankapp/BankApp.java
@@ -16,6 +16,15 @@ public class BankApp {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Bank person1 = new Bank();
+        person1.fullName = "Daniel Chidera";
+        person1.address = "Emene Enugu";
+        person1.bvn = 123456;
+        person1.dateOfBirth = "1 January 1999";
+        person1.email = "daniel@gmail.com";
+        person1.balance = 10000;
+        
+        person1.withdraw(9000);
     }
     
 }
